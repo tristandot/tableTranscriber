e8383d80a5bda0ddad34f1ab9470c47c6764b4d3
tristandot <tdot@dishas-ia>
2022-06-30 17:19:21 +0200

    modification script download.sh

--- download.sh
+++ download.sh
@@ -1,6 +1,6 @@
 set -e
 scripts/download_synthetic_resource.sh
-#scripts/download_wikiart.sh
+scripts/download_wikiart.sh
 scripts/download_seg_models.sh
 scripts/download_htr_model.sh
 scripts/download_dishastables.sh
