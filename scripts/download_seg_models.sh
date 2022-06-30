881c3adbe7b3042bb6ec2092adb041b531f444ad
tristandot <tdot@dishas-ia>
2022-06-30 20:02:17 +0200

    script update

--- scripts/download_seg_models.sh
+++ scripts/download_seg_models.sh
@@ -1,4 +1,4 @@
 set -e
-wget 'https://sharedocs.huma-num.fr/wl/?id=lKSGOeOoXvjIDjlaRQqUdyx2wHXBBZri&fmode=download' --output-document models.zip
+wget 'https://sharedocs.huma-num.fr/wl/?id=U7vDeozr8nuqr9z23uSc9B4AY7gWhXyI&fmode=download' --output-document models.zip
 unzip models.zip && rm models.zip
 mv models tableExtractor/
